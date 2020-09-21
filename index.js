dd1d642fde1f768fdb29955cff6a33951876cb8b
Ryan Block <rblock@gmail.com>
2020-09-21 12:51:16 -0600

    hi

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let test = require('tape')
 let sandbox = require('@architect/sandbox')
 
+test('Start sandbox', async t=> {
+  t.plan(1)
+  let result = await sandbox.start()
+  t.equal(result, 'Sandbox successfully started')
+})
+
 test('Shut down sandbox', async t=> {
   t.plan(1)
   let result = await sandbox.end()
